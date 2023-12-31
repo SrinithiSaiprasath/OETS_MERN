daa59fa2a9c9bd153091ff77c833b83aa3811e5f
Naveen <naveenrajurenu03@gmail.com>
2023-12-31 20:42:42 +0530

    firstcommit

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,76 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import {useNavigate} from'react-router-dom';
-import './Signup1.css';
+// Signup.js
 
-function Signup{
-  return(
+import React from 'react';
+import './Signup1.css'; // Make sure to adjust the path based on your project structure
+
+function Signup() {
+  return (
     <div>
       <html lang="en" dir="ltr">
         <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Registration</title>
         </head>
-      <body>
-        <div class="container">
-          <div class="title">Registration</div>
-          <div class="content">
-            <form action="#">
-              <div class="user-details">
-                <div class="input-box">
-                  <span class="details">Full Name</span>
-                  <input type="text" placeholder="Enter your name" required>
+        <body>
+          <div className="container">
+            <div className="title">Registration</div>
+            <div className="content">
+              <form action="#">
+                <div className="user-details">
+                  <div className="input-box">
+                    <span className="details">Full Name</span>
+                    <input type="text" placeholder="Enter your name" required />
+                  </div>
+                  <div className="input-box">
+                    <span className="details">Username</span>
+                    <input type="text" placeholder="Enter your username" required />
+                  </div>
+                  <div className="input-box">
+                    <span className="details">Email</span>
+                    <input type="text" placeholder="Enter your email" required />
+                  </div>
+                  <div className="input-box">
+                    <span className="details">Phone Number</span>
+                    <input type="text" placeholder="Enter your number" required />
+                  </div>
+                  <div className="input-box">
+                    <span className="details">Password</span>
+                    <input type="password" placeholder="Enter your password" required />
+                  </div>
+                  <div className="input-box">
+                    <span className="details">Confirm Password</span>
+                    <input type="password" placeholder="Confirm your password" required />
+                  </div>
                 </div>
-                <div class="input-box">
-                  <span class="details">Username</span>
-                  <input type="text" placeholder="Enter your username" required>
+                <div className="gender-details">
+                  <input type="radio" name="gender" id="dot-1" />
+                  <input type="radio" name="gender" id="dot-2" />
+                  <input type="radio" name="gender" id="dot-3" />
+                  <span className="gender-title">Gender</span>
+                  <div className="category">
+                    <label htmlFor="dot-1">
+                      <span className="dot one"></span>
+                      <span className="gender">Male</span>
+                    </label>
+                    <label htmlFor="dot-2">
+                      <span className="dot two"></span>
+                      <span className="gender">Female</span>
+                    </label>
+                    <label htmlFor="dot-3">
+                      <span className="dot three"></span>
+                      <span className="gender">Other</span>
+                    </label>
+                  </div>
                 </div>
-                <div class="input-box">
-                  <span class="details">Email</span>
-                  <input type="text" placeholder="Enter your email" required>
+                <div className="button">
+                  <input type="submit" value="Register" onClick={() => { window.location.href = 'afterregister.html'; }} />
                 </div>
-                <div class="input-box">
-                  <span class="details">Phone Number</span>
-                  <input type="text" placeholder="Enter your number" required>
-                </div>
-                <div class="input-box">
-                  <span class="details">Password</span>
-                  <input type="text" placeholder="Enter your password" required>
-                </div>
-                <div class="input-box">
-                  <span class="details">Confirm Password</span>
-                  <input type="text" placeholder="Confirm your password" required>
-                </div>
-              </div>
-              <div class="gender-details">
-                <input type="radio" name="gender" id="dot-1">
-                <input type="radio" name="gender" id="dot-2">
-                <input type="radio" name="gender" id="dot-3">
-                <span class="gender-title">Gender</span>
-                <div class="category">
-                  <label for="dot-1">
-                  <span class="dot one"></span>
-                  <span class="gender">Male</span>
-                </label>
-                <label for="dot-2">
-                  <span class="dot two"></span>
-                  <span class="gender">Female</span>
-                </label>
-                <label for="dot-3">
-                  <span class="dot three"></span>
-                  <span class="gender">Other</span>
-                  </label>
-                </div>
-              </div>
-              <div class="button">
-                <input type="submit" value="Register" onclick = "window.location.href='afterregister.html';">
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
-        </div>
-      </body>
+        </body>
       </html>
     </div>
-  )
+  );
 }
-export default Signup
+
+export default Signup;
--- client/src/components/success/successpage.js
+++ client/src/components/success/successpage.js
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import {useNavigate} from'react-router-dom';
-import './style1.css';
+import React from 'react';
+import './style1.css'; // Make sure to adjust the path based on your project structure
 
-
-function successpage{
-    return (
-        <div>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Pop up design</title>
-            </head>
-            <body>
-                <div class="container">
-                    <button type="submit" class="btn">Submit</button>
-                    <div class="popup">
-                        <img src="Tick1.png">
-                        <!-- <div class="tenor-gif-embed" data-postid="17807534" data-share-method="host" data-aspect-ratio=".5625" data-width="200%"><a href="https://tenor.com/view/confetti-sticker-falling-confetti-festivities-fiesta-gif-17807534">Confetti Sticker GIF</a>from <a href="https://tenor.com/search/confetti-gifs">Confetti GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> -->
-                        <h2>Thank You!</h2>
-                        <p>Your details has been successfully submitted.Thanks!!</p>
-                        <a href="mapsfinal.html"><button type="button">Track Your Order</button></a>
-                    </div>
-                </div>
-                
-            </body>
-            </html>
+function SuccessPage() {
+  return (
+    <div>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Pop up design</title>
+        </head>
+        <body>
+          <div className="container">
+            <button type="submit" className="btn">
+              Submit
+            </button>
+            <div className="popup">
+              <img src="Tick1.png" alt="tick-icon" />
+              <h2>Thank You!</h2>
+              <p>Your details have been successfully submitted. Thanks!!</p>
+              <a href="mapsfinal.html">
+                <button type="button">Track Your Order</button>
+              </a>
+            </div>
+          </div>
+        </body>
+      </html>
     </div>
-    )
+  );
 }
 
-export default successpage
+export default SuccessPage;
